1f9b4c419464091df960d63eeaffa5093af60c51
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-05-05 22:20:06 +0200

    env dev

--- backend/src/data/demo-attandance.js
+++ backend/src/data/demo-attandance.js
@@ -1,3 +1,16 @@
 var attandance = {
-    
-}
+  TimeCardRepor: {
+    date: row.date,
+    name: row.name,
+    userId: row.user_id,
+    department: "",
+    shift: row.shift_name,
+    firstTime: {
+      clockInDefault: "7:00:00",
+      clockIn: "7:00:00",
+      clockOut: "7:00:00",
+      timeLate: "00:00:00",
+      clockOutDefault: "7:00:00",
+    },
+  },
+};

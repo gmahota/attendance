c0db907eae0229d5c7bb3e7b7e2543dbb5323bec
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-05-19 23:13:00 +0200

    disponibilizacao pasta uploads

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -62,6 +62,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(express.static('./public'))
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(errorHandler);
 app.use(morgan('combined'))
